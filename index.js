729d3bca7e862763df03c25f194a3b3240afa924
dimitarkolev <dk@almero.bg>
2023-01-31 07:37:33 +0000

    Bump

--- index.js
+++ index.js
@@ -3,31 +3,30 @@ import { Server } from 'socket.io'
 
 export default function (options = {}) {
     const { port, server = createServer(), token, logger = console } = options
-    const io = options.io || new Server(server, {})
-    let triggerUri
 
-    const mikserNamespace = io.of('/mikser')
+    const mikserNamespace = new Server(server).of('/mikser')
     mikserNamespace.use((socket, next) => {
         if (!token) return next()
-        if (socket.handshake.query.token != token) {
-            next(new Error('Authorization token is missing or invalid.'))
+        if (socket.handshake.auth?.token != token) {
+            return next(new Error('Authorization token is missing or invalid.'))
         }
+        next()
     })
-    mikserNamespace.on('connection', (socket) => {
-        if (triggerUri) {
-            logger.info('Mikser socket trigger: %s', triggerUri)
-            socket.emit('trigger', { uri: triggerUri })
-        }
+    mikserNamespace.on('connection', () => {
+        logger.info('Mikser subscription connected')
     })
 
     if (options.port && !options.server) {
         server.listen(port)
+        logger.info('Mikser subscription initalized: %d', port)
+    } else {
+        logger.info('Mikser subscription initalized')
     }
-    logger.info('Mikser socket initalized')
 
     return {
         async trigger(uri) {
             triggerUri = uri
+            mikserNamespace.emit('trigger', { uri })
         },
         async created(name, entity) {
             logger.info('Mikser socket created: %s %s', name, entity.id)
